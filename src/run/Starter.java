6220d642f4fbe2d3d71ff70eb96b6837c665ddc3
Esteban <estebanpw@uma.es>
2017-02-28 22:05:39 +0100

    First test agent working and parameter passing in constructor

--- src/run/Starter.java
+++ src/run/Starter.java
@@ -25,13 +25,14 @@ public class Starter {
 		Board b = new Board(x, y);
 		Frame f = new Frame();
 		
-		
 		//Initialize frame and board
 		f.start(b, pixelsPerCell);
 		
 		//Create thread to run the system through iterations
 		Stepper stp = new Stepper(b, f, maxCycles);
 		
+		//Manager to handle board, frame and stepper at once
+		Manager m = new Manager(f, b, stp);
 		
 		//Launch container
 
@@ -40,26 +41,35 @@ public class Starter {
 
 		//Exit the JVM when there are no more containers around
 		rt.setCloseVM(true);
-		f.send_info("JADE-Runtime created", stp.get_time());
+		m.deliver_message("JADE-Runtime created");
 
 		//Create a default profile
 		int port = 1200;
 		Profile profile = new ProfileImpl(null, port, null);
-		f.send_info("JADE-Created profile on port "+port, stp.get_time());
+		m.deliver_message("JADE-Created profile on port "+port);
 
 		
-		f.send_info("JADE-Launched platform", stp.get_time());
+		m.deliver_message("JADE-Launched platform");
 		jade.wrapper.AgentContainer mainContainer = rt.createMainContainer(profile);
 
 		//Now set the default Profile to start a container
 		ProfileImpl pContainer = new ProfileImpl(null, 1200, null);
 		jade.wrapper.AgentContainer cont = rt.createAgentContainer(pContainer);
-		f.send_info("JADE-Container created", stp.get_time());
+		m.deliver_message("JADE-Container created");
 
-		
+		/*
 		AgentController rma = mainContainer.createNewAgent("rma","jade.tools.rma.rma", new Object[0]);
-		f.send_info("JADE-Launching Remote Management Agent", stp.get_time());
+		m.deliver_message("JADE-Launching Remote Management Agent");
 		rma.start();
+		*/
+		
+		//Parameters as objects for the constructor
+		Object objs[] = new Object[1];
+		objs[0] = (Object) m;
+		
+		//Create test agent 
+		AgentController ag1 = mainContainer.createNewAgent("John", "agents.TestAgent", objs);
+		ag1.start();
 		
 		//Start thread runner
 		stp.run();
